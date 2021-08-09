1d200edc103dcc229dbd652649fef29a2136b227
Vekaz <V4nII@hotmail.com>
2021-08-09 22:39:20 +0200

    Add Base class for Instrumentation tests
    Test Base class implements login and logout before running tests.

---
+++ app/src/androidTest/java/ap/mailo/BaseUITest.java
@@ -0,0 +1,71 @@
+package ap.mailo;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Context;
+import android.util.Log;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+import ap.mailo.auth.LoginNetwork;
+import ap.mailo.auth.LoginResult;
+import ap.mailo.auth.LoginViewModel;
+
+public abstract class BaseUITest
+{
+
+    protected static Context appContext;
+    protected static Context testContext;
+    protected static LoginViewModel loginViewModel;
+    protected static LoginResult loginResult;
+
+    protected static String mail;
+    protected static String password;
+
+    protected static LoginResult getLoginResult() {
+        return loginResult;
+    }
+
+    public static void loadCredentialsFromJSON() throws JSONException
+    {
+        String jsonString = testContext.getClassLoader().getResource("account.json").toString();
+
+        JSONObject obj = new JSONObject(jsonString);
+        mail = obj.getString("mail");
+        password = obj.getString("password");
+    }
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception
+    {
+        appContext = ApplicationProvider.getApplicationContext();
+        testContext = InstrumentationRegistry.getInstrumentation().getContext();
+        loginViewModel = new LoginViewModel();
+
+        loadCredentialsFromJSON();
+        LoginNetwork.loginUser(mail, password).subscribe(result -> loginResult = result);
+        assert getLoginResult().getSuccess() != null;
+        loginViewModel.finishLogin(getLoginResult().getSuccess(), appContext);
+
+        Log.INFO("Logged in before test cases");
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception
+    {
+        AccountManager am = AccountManager.get(appContext);
+        Account[] accounts = am.getAccounts();
+        if (accounts.length > 0) {
+            Account accountToRemove = accounts[0];
+            am.removeAccount(accountToRemove, null, null, null);
+            Log.INFO("Logged out after test cases");
+        }
+    }
+
+}
---
+++ app/src/androidTest/java/ap/mailo/MainActivityTests.java
@@ -0,0 +1,14 @@
+package ap.mailo;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTests extends BaseUITest
+{
+
+}

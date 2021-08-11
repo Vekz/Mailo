ded00b119ade2e2f5311a807bb475dd6d3f2880f
Vekaz <V4nII@hotmail.com>
2021-08-12 00:29:01 +0200

    Added basic test cases to check Composing mails fragment
    Unfortunately in SDK31 testing toasts doesn't really work, so I can't check more cases.

--- app/src/androidTest/java/ap/mailo/MainActivityTests.java
+++ app/src/androidTest/java/ap/mailo/MainActivityTests.java
@@ -2,6 +2,7 @@ package ap.mailo;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.Root;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -13,13 +14,17 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.swipeUp;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isNotEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -69,22 +74,72 @@ public class MainActivityTests extends BaseUITest
         onView(withId(R.id.FolderTitle)).check(matches(withText("Drafts")));
     }
 
-    //Matchers
-    private static Matcher<View> childAtPosition(final Matcher<View> parentMatcher, final int position) {
+    @Test
+    public void composeCorrectMailAndSend() throws InterruptedException {
 
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
+        onView(withId(R.id.fab)).perform(click());
 
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
+        onView(withId(R.id.writeSubject)).perform(typeText("Some title"));
+        onView(withId(R.id.writeTo)).perform(typeText("example@example.com"));
+        onView(withId(R.id.writeContent)).perform(typeText("Some content"));
+
+        closeSoftKeyboard();
+
+        onView(withId(R.id.fab)).perform(click());
+
+        Thread.sleep(2000);
+
+        //isToastMessageDisplayed(R.string.succes_sending); Check is not working in SDK31
+        onView(withId(R.id.FolderTitle)).check(matches(isDisplayed()));
     }
+
+    @Test
+    public void composeIncorrectMail() throws InterruptedException S{
+
+        onView(withId(R.id.fab)).perform(click());
+
+        onView(withId(R.id.writeSubject)).perform(typeText("Some title"));
+        onView(withId(R.id.writeTo)).perform(typeText("example@"));
+        onView(withId(R.id.writeContent)).perform(typeText("Some content"));
+
+        closeSoftKeyboard();
+
+        onView(withId(R.id.fab)).check(matches(isNotEnabled()));
+
+        onView(withId(R.id.fab)).perform(click());
+
+        Thread.sleep(2000);
+
+        onView(withId(R.id.FolderTitle)).check(doesNotExist());
+    }
+
+    //Matchers
+
+        //Toast helpers
+        public static Matcher<Root> isToast() {
+            return new ToastMatcher();
+        }
+
+        public void isToastMessageDisplayed(int textId) {
+            onView(withText(textId)).inRoot(isToast()).check(matches(isDisplayed()));
+        }
+
+        //Child at position helpers
+        private static Matcher<View> childAtPosition(final Matcher<View> parentMatcher, final int position) {
+
+            return new TypeSafeMatcher<View>() {
+                @Override
+                public void describeTo(Description description) {
+                    description.appendText("Child at position " + position + " in parent ");
+                    parentMatcher.describeTo(description);
+                }
+
+                @Override
+                public boolean matchesSafely(View view) {
+                    ViewParent parent = view.getParent();
+                    return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                            && view.equals(((ViewGroup) parent).getChildAt(position));
+                }
+            };
+        }
 }
